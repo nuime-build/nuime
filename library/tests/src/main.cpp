1d582caca835fc5d5c1094e024d9e95e1b085f70
Xavier Leclercq <xavier@leclercq.dev>
2026-07-29 13:19:35 +0100

    Add library tests (#9)

---
+++ library/tests/src/main.cpp
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2026 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#include "EngineTests.hpp"
+#include <Nuime/BuildFiles/linkoptions.hpp>
+#include <Nuime/linkoptions.hpp>
+#include <Ishiko/TestFramework.hpp>
+#include <exception>
+
+using namespace Ishiko;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        TestHarness::CommandLineSpecification command_line_spec;
+        Configuration configuration = command_line_spec.createDefaultConfiguration();
+        configuration.set("context.data", "../../data");
+        configuration.set("context.reference", "../../reference");
+        configuration.set("context.output", "../../output");
+        CommandLineParser::parse(command_line_spec, argc, argv, configuration);
+
+        TestHarness the_test_harness("Nuime Library Tests", configuration);
+
+        TestSequence& the_tests = the_test_harness.tests();
+        the_tests.append<EngineTests>();
+
+        return the_test_harness.run();
+    }
+    catch (const std::exception& e)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+    catch (...)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+}
