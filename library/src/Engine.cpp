3d4c20268c99a7f672214b5e3de2c6d3392ffc67
Xavier Leclercq <xavier@leclercq.dev>
2026-07-29 12:38:10 +0100

    Updates

--- library/src/Engine.cpp
+++ library/src/Engine.cpp
@@ -2,3 +2,16 @@
 // SPDX-License-Identifier: MIT
 
 #include "Engine.hpp"
+
+using namespace Nuime;
+
+void Engine::load(const boost::filesystem::path& path, Ishiko::Error& error)
+{
+    m_build_file.load(path, error);
+}
+
+void Engine::exportToCMake(const boost::filesystem::path& output_path, Ishiko::Error& error)
+{
+    // TODO: project the loaded model onto a CMakeLists.txt. The engine decides how to map the nuime
+    // model onto CMake; for now this will handle our single static-library example.
+}
